bd0b24cb3c83d647fda6aaf01300592466d444d4
curryan7 <bk249@byu.edu>
2024-03-18 15:58:18 -0600

    EMPTY creates spaces by default. Removed EMPTY from the margin function, and fixed margin numbers along with customizing by orientation.

--- client/src/main/java/ui/ChessDesign.java
+++ client/src/main/java/ui/ChessDesign.java
@@ -12,22 +12,32 @@ public class ChessDesign {
     private static final int BOARD_SIZE_IN_CHARS = 31;
     private static final int BOARD_SIZE_IN_SQUARES = 10;
     private static final int SQUARE_SIZE_IN_CHARS = 3;
-    private static Random rand = new Random();
 
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
         out.print(ERASE_SCREEN);
-
-        drawThemHorizontalAxis(out);
-        drawChessBoard(out);
+        
+        drawThemHorizontalAxis(out, 2);
+        drawChessBoard(out,2);
+        drawThemHorizontalAxis(out, 2);
 
     }
 
-    private static void drawThemHorizontalAxis(PrintStream out) {
-        String[] headers = {"   ", " h ", " g ", " f ", " e ", " d ", " c ", " b ", " a ", "   "};
-        for (int boardCol = 0; boardCol< BOARD_SIZE_IN_SQUARES; ++boardCol){
-            drawHorizontalAxis(out, headers[boardCol]);
+
+    private static void drawThemHorizontalAxis(PrintStream out, int orientation) {
+        if (orientation ==1 ) {
+            String[] headers = {"   ", " h ", " g ", " f ", " e ", " d ", " c ", " b ", " a ", "   "};
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                drawHorizontalAxis(out, headers[boardCol]);
+            }
         }
+        else
+            if(orientation == 2){
+                String[] headers = {"   ", " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ", "   "};
+                for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                    drawHorizontalAxis(out, headers[boardCol]);
+                }
+            }
         System.out.println(RESET_BG_COLOR);
     }
 
@@ -35,31 +45,82 @@ public class ChessDesign {
         printHeaderText(out, headerText);
     }
 
-    private static void drawChessBoard(PrintStream out){
-        String[] siders = {" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "};
-        int i = 0;
-        int j = 0;
+    private static void drawChessBoard(PrintStream out, int orientation) {
+        for (int boardRow = 0; boardRow <= 7; ++boardRow) {
+            for (int boardCol = 0; boardCol <= 9; ++boardCol) {
+                if (orientation == 1){
 
-        for (int boardRow = 1; boardRow <= 9; ++boardRow){
-            for (int boardCol = 1; boardCol <= BOARD_SIZE_IN_SQUARES; ++boardCol){
-                if (boardCol ==1 | boardCol == 10){
-                    setGray(out);
                 }
-                else if ((boardRow+boardCol)%2 == 0){
+                    if (boardCol == 0 || boardCol == 9) {
+                        if (orientation == 1){
+                            switch(boardRow){
+                                case 0:
+                                    printerMarginText(out, " 1 ");
+                                    break;
+                                case 1:
+                                    printerMarginText(out, " 2 ");
+                                    break;
+                                case 2:
+                                    printerMarginText(out, " 3 ");
+                                    break;
+                                case 3:
+                                    printerMarginText(out, " 4 ");
+                                    break;
+                                case 4:
+                                    printerMarginText(out, " 5 ");
+                                    break;
+                                case 5:
+                                    printerMarginText(out, " 6 ");
+                                    break;
+                                case 6:
+                                    printerMarginText(out, " 7 ");
+                                    break;
+                                case 7:
+                                    printerMarginText(out, " 8 ");
+                                    break;
+                            }
+                        }
+                        else if (orientation == 2){
+                            switch(boardRow){
+                                case 0:
+                                    printerMarginText(out, " 8 ");
+                                    break;
+                                case 1:
+                                    printerMarginText(out, " 7 ");
+                                    break;
+                                case 2:
+                                    printerMarginText(out, " 6 ");
+                                    break;
+                                case 3:
+                                    printerMarginText(out, " 5 ");
+                                    break;
+                                case 4:
+                                    printerMarginText(out, " 4 ");
+                                    break;
+                                case 5:
+                                    printerMarginText(out, " 3 ");
+                                    break;
+                                case 6:
+                                    printerMarginText(out, " 2 ");
+                                    break;
+                                case 7:
+                                    printerMarginText(out, " 1 ");
+                                    break;
+                            }
+                        }
+                }
+
+                else if ((boardRow + boardCol) % 2 == 0) {
                     setWhite(out);
-                }
-                else {
+                    out.print(EMPTY);
+                } else {
                     setBlack(out);
+                    out.print(EMPTY);
                 }
 
-                out.print(EMPTY);
                 out.print(RESET_BG_COLOR);
             }
-
             out.println();
-            if (boardRow == 9){
-                drawThemHorizontalAxis(out);
-            }
         }
     }
 
